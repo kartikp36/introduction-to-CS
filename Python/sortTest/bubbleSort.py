7dac71f8a9c0905926dad9721d64c1fd294e216d
kartikp36 <kartikp2002@gmail.com>
2021-01-24 22:03:17 +0530

    fix: lint sortTest

--- Python/sortTest/bubbleSort.py
+++ Python/sortTest/bubbleSort.py
@@ -1,13 +1,13 @@
-def bubbleSort(arr): 
-    n = len(arr) 
-    for i in range(n): 
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
         swapped = False
-  
-        for j in range(0, n-i-1): 
-            if arr[j] > arr[j+1] : 
-                arr[j], arr[j+1] = arr[j+1], arr[j] 
+
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
-        if swapped == False: 
+        if not swapped:
             break
-    
+
     return arr
